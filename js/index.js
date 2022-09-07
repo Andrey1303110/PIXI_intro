45cee52f10c64a127220b2cbf994cd413dbd0fa5
andrey1303110 <andrey1303110@gmail.com>
2022-09-07 16:29:38 +0300

    add assets with Map

--- js/index.js
+++ js/index.js
@@ -1,8 +1,32 @@
+import { assetsMap } from "./assetsMap.js";
 import * as PIXI from "./pixi.mjs";
 
 const app = new PIXI.Application({
     width: 1280,
     height: 720,
+    backgroundColor: 0xc2c2c2
 });
 
 document.body.appendChild(app.view);
+
+const runGame = () => {
+    const marker = new PIXI.Graphics();
+    let params = {
+        x: 1/3,
+        y: 1/3,
+        width: .5,
+        height: .5
+    };
+    marker
+    .beginFill(0xff0000, .45)
+    .drawCircle(0, 0, 25)
+    .endFill();
+
+    app.stage.addChild(marker);
+
+    app.stage.position.set(1280/2, 720/2);
+}
+
+assetsMap.sprites.forEach((value) => app.loader.add(value.name, value.url));
+app.loader.load(runGame);
+
