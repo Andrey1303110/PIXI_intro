032f72f979a66eb88eddd67baf17f28fdf80fdb1
andrey1303110 <andrey1303110@gmail.com>
2022-09-08 11:18:57 +0300

    delete unusable

--- js/index.js
+++ js/index.js
@@ -13,21 +13,10 @@ document.body.appendChild(app.view);
 
 const runGame = () => {
     const marker = new PIXI.Graphics();
-    let params = {
-        x: 1/3,
-        y: 1/3,
-        width: .5,
-        height: .5
-    };
-    marker
-    .beginFill(0xff0000, .45)
-    .drawCircle(0, 0, 25)
-    .endFill();
 
     const tank = new Tank();
     
     app.stage.addChild(tank.view);
-    app.stage.addChild(marker);
 
     app.stage.position.set(1280/2, 720/2);
 
